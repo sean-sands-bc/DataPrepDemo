6bbcacdca3554a2a5818a4b8bd312b39b350c9b4
sean.sands <sean.sands@bellevuecollege.edu>
2021-02-06 05:14:00 -0800

    a message

--- DataPrepDemo/DataPrepDemo.cpp
+++ DataPrepDemo/DataPrepDemo.cpp
@@ -1,47 +1,118 @@
 // DataPrepDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
 
 #include <iostream>
 #include <fstream>
+#include <string>
+//#include <filesystem>
+#include <experimental/filesystem>
 
-int main()
+
+void extractFields()
 {
     const int eventSize = 0x68;
+
+    //std::filesystem::directory_entry("/tdg");
+    auto gyro = std::experimental::filesystem::directory_entry("./gyro");
+    std::cout << gyro.path().generic_string() << std::endl;
+    for (auto& entry : std::experimental::filesystem::directory_iterator("./tdg"))
+    {   
+        std::cout << entry.path().generic_string() << std::endl;
+        //std::cout << gyro.path().generic_string() +"/"+ entry.path().filename().replace_extension(".gyro").generic_string() << std::endl;
+        std::cout << ( gyro.path() / (entry.path().filename().replace_extension(".gyro") ) ).generic_string() << std::endl;
+        
+        std::ifstream ifs;
+        //ifs.open("loggerdata.txt", std::ifstream::binary);
+        ifs.open(entry.path().generic_string(), std::ifstream::binary);
+        char buf[eventSize];
+
+        std::ofstream ofs;
+        //ofs.open("prepareddata.txt", std::ifstream::binary);
+        ofs.open((gyro.path() / (entry.path().filename().replace_extension(".gyro"))).generic_string(), std::ifstream::binary);
+        int i = 0;
+        //std::cout << ifs.good() << ifs.eof() << ifs.fail() << ifs.bad() << std::endl;
+//        unsigned long long t1=0;
+//        unsigned long long t2;
+        while (ifs.read(buf, eventSize))
+        {
+            ++i;
+            if (buf[0] != 0x68) // check for size signature
+            {
+                std::cout << i << std::endl;
+                break;
+            }
+
+
+            //float x = *(float*)(buf + 0x18);
+            //float y = *(float*)(buf + 0x1c);
+            //float z = *(float*)(buf + 0x20);
+//            t2 = *(unsigned long long*)(buf + 0x10);
+            //sensor type here int s = *(int*)(buf + 0x08);
+            //std::cout << x << ' ' << y << ' ' << z << std::endl;
+//            std::cout << (t2-t1) << std::endl;
+            ofs.write(buf + 0x10, 0x14); // write timestamp and sensor data
+//            t1 = t2;
+            
+        }
+        ifs.close();
+        ofs.close();
+            
+    }
+        //std::cout << i << std::endl;
+        //std::cout << ifs.good() << ifs.eof() << ifs.fail() << ifs.bad() << std::endl;
+        
+
+}
+
+void normalizeFields()
+{
+    const int eventSize = 0x14;
     std::ifstream ifs;
-    ifs.open("loggerdata.txt", std::ifstream::binary);
+    ifs.open("prepareddata.txt", std::ifstream::binary);
     char buf[eventSize];
 
     std::ofstream ofs;
-    ofs.open("prepareddata.txt",std::ifstream::binary);
-    int i = 0;
-    std::cout << ifs.good() << ifs.eof() << ifs.fail() << ifs.bad() << std::endl;
+    ofs.open("normalizeddata.txt", std::ifstream::binary);
+
     while (ifs.read(buf, eventSize))
     {
-        ++i;
-        if (buf[0] != eventSize)
-        {
-            break;
-        }
+        //++i;
+        //long t = *(long*)(buf);
+        float x = *(float*)(buf + 0x8);
+        float y = *(float*)(buf + 0xc);
+        float z = *(float*)(buf + 0x10);
         
-        
-        float x = *(float*)(buf + 0x18);
-        float y = *(float*)(buf + 0x1c);
-        float z = *(float*)(buf + 0x20);
-        long t = *(long*)(buf + 0x10);
         //sensor type here int s = *(int*)(buf + 0x08);
         std::cout << x << ' ' << y << ' ' << z << std::endl;
-        ofs.write(buf + 0x10, 0x14); // write timestamp and sensor data
+
+        float mag = sqrtf(x * x + y * y + z * z);
+        float xn = x / mag;
+        float yn = y / mag;
+        float zn = z / mag;
+
+        std::cout << xn << ' ' << yn << ' ' << zn << ' ' << mag << std::endl;
+
+        ofs.write(buf, 0x8); // write timestamp
+        ofs.write((char*)&mag,4);
+        ofs.write((char*)&xn, 4);
+        ofs.write((char*)&yn, 4);
+        ofs.write((char*)&zn, 4);
         
-
     }
-    std::cout << i << std::endl;
-    std::cout << ifs.good() << ifs.eof() << ifs.fail() << ifs.bad() << std::endl;
-    ifs.close();
-    ofs.close();
+}
 
+int main()
+{
     
+
+    extractFields();
+    //normalizeFields();
+
+    //char c;
+    //std::cin >> c;
     //ofs.write(buf, 104);
-    
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
